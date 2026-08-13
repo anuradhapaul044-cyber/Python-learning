df4e147639cd6b102754b01bef75d6f4ea72e24e
anuradhapaul044-cyber <anuradhapaul044@gmail.com>
2026-08-13 12:35:58 +0530

    Variable and Data Types

---
+++ Variable.py
@@ -0,0 +1,19 @@
+#Python Basics-Variables and Data Types
+#1. Integer 
+age=10
+# 2. Float
+height=5.9
+# 3. String 
+name="John"
+# 4. Boolean
+is_student=True
+# Printing the variables
+print("Age:", age)
+print("Height:", height)
+print("Name:", name)
+print("Is Student:", is_student)
+# Checking variable type 
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))

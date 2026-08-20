4904ba360dd3b582ede8cdc960bf9f24baa2c7dc
taaraa <abc@yahoo.com>
2026-08-20 21:04:16 +0530

    random_numbers.py

---
+++ random_number.py
@@ -0,0 +1,6 @@
+#python does not have a random() function to print random numbers but it has a built-in module called random using which we can generate random numbers.
+import random
+#randarange() function dosen't include the upper limit in the range of numbers to be generated. 
+print(random.randrange(1, 646543465))
+# random.randint() function includes the upper limit in the range of numbers to be generated.
+print(random.randint(1,10)) 

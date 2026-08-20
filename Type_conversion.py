acc3c0690a28d4206c7f5e68cd02686ad6cf71c6
taaraa <abc@yahoo.com>
2026-08-20 21:43:58 +0530

    type conversion

---
+++ Type_conversion.py
@@ -0,0 +1,29 @@
+age=input("enter your age:")
+cgpa=input("enter your cgpa:")
+#type checking
+print(type(age))
+print(type(cgpa))
+#convert a string input to numbers
+age=int(age)
+cgpa=float(cgpa)
+
+#type checking after conversion
+print(type(age))
+print(type(cgpa))
+# some other conversions
+a = 100
+
+# type checking
+print(type(a))
+
+# converting a to string
+a = str(a)
+
+# type checking after conversion to string
+print(type(a))
+
+# converting a to a float
+a = float(a)
+
+# type checking after conversion to float
+print(type(a))
